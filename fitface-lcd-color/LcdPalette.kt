cb30b3e226a44a69999d7d1f81ae4632a39fceea
q30511313-prog <q30511313@gmail.com>
2026-08-25 06:12:29 +0900

    Add approved cool LCD silver palette

---
+++ fitface-lcd-color/LcdPalette.kt
@@ -0,0 +1,9 @@
+package dev.fitface.studio.core.model
+
+/** Approved cool-neutral LCD silver for the Casio-style Fit3 faces. */
+object LcdPalette {
+    val SILVER_ARGB: Int = 0xFFAEB4B2.toInt()
+    const val SILVER_RED: Int = 0xAE
+    const val SILVER_GREEN: Int = 0xB4
+    const val SILVER_BLUE: Int = 0xB2
+}
