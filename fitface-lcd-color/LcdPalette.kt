c1d2e6d05dd677ddd415b7a62dc7f106b15f35a6
q30511313-prog <q30511313@gmail.com>
2026-08-25 07:44:05 +0900

    Use sampled Casio LCD gray #9F9E99

--- fitface-lcd-color/LcdPalette.kt
+++ fitface-lcd-color/LcdPalette.kt
@@ -1,9 +1,9 @@
 package dev.fitface.studio.core.model
 
-/** Approved cool-neutral LCD silver for the Casio-style Fit3 faces. */
+/** Approved warm-neutral LCD gray sampled from the final Casio-style reference face. */
 object LcdPalette {
-    val SILVER_ARGB: Int = 0xFFAEB4B2.toInt()
-    const val SILVER_RED: Int = 0xAE
-    const val SILVER_GREEN: Int = 0xB4
-    const val SILVER_BLUE: Int = 0xB2
+    val SILVER_ARGB: Int = 0xFF9F9E99.toInt()
+    const val SILVER_RED: Int = 0x9F
+    const val SILVER_GREEN: Int = 0x9E
+    const val SILVER_BLUE: Int = 0x99
 }
