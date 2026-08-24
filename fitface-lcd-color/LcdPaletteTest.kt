27c79595ad99392561fbc10bfd462752f319a54e
q30511313-prog <q30511313@gmail.com>
2026-08-25 06:03:57 +0900

    Add failing approved LCD silver palette test

---
+++ fitface-lcd-color/LcdPaletteTest.kt
@@ -0,0 +1,14 @@
+package dev.fitface.studio.core.format
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class LcdPaletteTest {
+    @Test
+    fun approvedSilverIsCoolNeutralAeb4b2() {
+        assertEquals(0xFFAEB4B2.toInt(), LcdPalette.SILVER_ARGB)
+        assertEquals(0xAE, LcdPalette.SILVER_RED)
+        assertEquals(0xB4, LcdPalette.SILVER_GREEN)
+        assertEquals(0xB2, LcdPalette.SILVER_BLUE)
+    }
+}
