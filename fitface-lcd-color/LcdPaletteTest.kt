8ac60792083054adabe0bc59cb85e8cfd0e05459
q30511313-prog <q30511313@gmail.com>
2026-08-25 07:44:16 +0900

    Test sampled Casio LCD gray

--- fitface-lcd-color/LcdPaletteTest.kt
+++ fitface-lcd-color/LcdPaletteTest.kt
@@ -6,10 +6,10 @@ import org.junit.Test
 
 class LcdPaletteTest {
     @Test
-    fun approvedSilverIsCoolNeutralAeb4b2() {
-        assertEquals(0xFFAEB4B2.toInt(), LcdPalette.SILVER_ARGB)
-        assertEquals(0xAE, LcdPalette.SILVER_RED)
-        assertEquals(0xB4, LcdPalette.SILVER_GREEN)
-        assertEquals(0xB2, LcdPalette.SILVER_BLUE)
+    fun approvedSilverMatchesFinalCasioReference9f9e99() {
+        assertEquals(0xFF9F9E99.toInt(), LcdPalette.SILVER_ARGB)
+        assertEquals(0x9F, LcdPalette.SILVER_RED)
+        assertEquals(0x9E, LcdPalette.SILVER_GREEN)
+        assertEquals(0x99, LcdPalette.SILVER_BLUE)
     }
 }
